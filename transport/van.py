612296e11fb4a799c4799ca3c088d98c248b588a
Bobtaiil <dbanacto@gmail.com>
2024-12-21 00:17:43 +0300

    Update van.py

--- transport/van.py
+++ transport/van.py
@@ -1,6 +1,6 @@
-from transport.vehicle import Vehicle  #Импортируем класс Vehicle из модуля transport.vehicle
+from transport.vehicle import Vehicle  #Импортируем класс Vehicle
 
-class Van(Vehicle):  #Определяем класс Van, который наследует класс Vehicle
+class Van(Vehicle):  #Определяем класс Van
     def __init__(self, capacity, is_refrigerated=False):  #Метод инициализации класса
-        super().__init__(capacity)  #Вызываем метод инициализации родительского класса Vehicle
-        self.is_refrigerated = is_refrigerated  #Инициализируем атрибут is_refrigerated значением параметра is_refrigerated (по умолчанию False)
+        super().__init__(capacity)  #Вызываем метод Vehicle
+        self.is_refrigerated = is_refrigerated  #Инициализируем атрибут is_refrigerated
