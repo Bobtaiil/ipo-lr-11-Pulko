77be47eca64b2d096d2db49717181c62e6f7ec73
Bobtaiil <dbanacto@gmail.com>
2024-12-21 00:16:44 +0300

    Update client.py

--- transport/client.py
+++ transport/client.py
@@ -1,1 +1,10 @@
+class Client:  #Определяем класс Client
+    def __init__(self, name, cargo_weight, is_vip=False):  #Инициализация класса
+        self.name = name  #name
+        self.cargo_weight = self.validate_cargo_weight(cargo_weight)  #cargo_weightо
+        self.is_vip = is_vip  #атрибут is_vip
 
+    def validate_cargo_weight(self, cargo_weight):  #Валидация груза
+        if not isinstance(cargo_weight, (int, float)) or cargo_weight <= 0:  #Проверка
+            raise ValueError("Вес груза должен быть положительным числом")
+        return cargo_weight  #Возвращаем валидированный вес груза
